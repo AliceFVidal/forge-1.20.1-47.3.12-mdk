f5fbf793e945f9175ed79dda828f93808baa6811
alice <alicefrancavidal@gmail.com>
2024-12-18 17:48:26 -0300

    New Fuel Added!

---
+++ src/main/java/net/idunna/oldschoolfantasy/item/custom/MetalDetectorItem.java
@@ -0,0 +1,11 @@
+package net.idunna.oldschoolfantasy.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class MetalDetectorItem extends Item {
+    public MetalDetectorItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+
+}
